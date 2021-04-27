2a9b8d781f4ad18efc703a939126062c1791f50e
Joel Kerfoot <joelkerfoot@gmail.com>
2021-04-26 22:48:41 -0700

    feat: first attempt at a custom Town NPC

---
+++ NPCs/ExamplePerson.cs
@@ -0,0 +1,135 @@
+﻿using BetterFishing.Items.Bait;
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace BetterFishing.NPCs
+{
+    [AutoloadHead]
+    class ExamplePerson : ModNPC
+    {
+        public override string Texture => "BetterFishing/NPCs/ExamplePerson";
+
+        public override string[] AltTextures => new[] { "BetterFishing/NPCs/ExamplePerson_Alt_1" };
+
+        public override bool Autoload(ref string name)
+        {
+            name = "Example Person";
+            return mod.Properties.Autoload;
+        }
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Example Person");
+            Main.npcFrameCount[npc.type] = 25;
+            NPCID.Sets.ExtraFramesCount[npc.type] = 9;
+            NPCID.Sets.AttackFrameCount[npc.type] = 4;
+            NPCID.Sets.DangerDetectRange[npc.type] = 700;
+            NPCID.Sets.AttackType[npc.type] = 0;
+            NPCID.Sets.AttackTime[npc.type] = 90;
+            NPCID.Sets.AttackAverageChance[npc.type] = 30;
+            NPCID.Sets.HatOffsetY[npc.type] = 4;
+        }
+
+        public override void SetDefaults()
+        {
+            npc.townNPC = true;
+            npc.friendly = true;
+            npc.width = 18;
+            npc.height = 40;
+            npc.aiStyle = 7;
+            npc.damage = 10;
+            npc.defense = 15;
+            npc.lifeMax = 250;
+            npc.HitSound = SoundID.NPCHit1;
+            npc.DeathSound = SoundID.NPCDeath1;
+            npc.knockBackResist = 0.5f;
+            animationType = NPCID.Guide;
+        }
+
+        public override bool CanTownNPCSpawn(int numTownNPCs, int money)
+        {
+            return true;
+        }
+
+        // Example Person needs a house built out of ExampleMod tiles. You can delete this whole method in your townNPC for the regular house conditions.
+        public override bool CheckConditions(int left, int right, int top, int bottom)
+        {
+            return true;
+        }
+
+        public override string TownNPCName()
+        {
+            switch (WorldGen.genRand.Next(4))
+            {
+                case 0:
+                    return "Someone";
+                case 1:
+                    return "Somebody";
+                case 2:
+                    return "Blocky";
+                default:
+                    return "Colorless";
+            }
+        }
+
+        public override string GetChat()
+        {
+            int partyGirl = NPC.FindFirstNPC(NPCID.PartyGirl);
+            if (partyGirl >= 0 && Main.rand.NextBool(4))
+            {
+                return "Can you please tell " + Main.npc[partyGirl].GivenName + " to stop decorating my house with colors?";
+            }
+            switch (Main.rand.Next(4))
+            {
+                case 0:
+                    return "Sometimes I feel like I'm different from everyone else here.";
+                case 1:
+                    return "What's your favorite color? My favorite colors are white and black.";
+                case 2:
+                    {
+                        // Main.npcChatCornerItem shows a single item in the corner, like the Angler Quest chat.
+                        Main.npcChatCornerItem = ItemID.HiveBackpack;
+                        return $"Hey, if you find a [i:{ItemID.HiveBackpack}], I can upgrade it for you.";
+                    }
+                default:
+                    return "What? I don't have any arms or legs? Oh, don't be ridiculous!";
+            }
+        }
+
+        public override void SetChatButtons(ref string button, ref string button2)
+        {
+            button = Language.GetTextValue("LegacyInterface.28");
+            button2 = "TODO";
+        }
+
+        public override void OnChatButtonClicked(bool firstButton, ref bool shop)
+        {
+            if (firstButton)
+            {
+                shop = true;
+            }
+             else
+            {
+                Main.npcChatText = "Oops";
+            }
+        }
+
+        public override void SetupShop(Chest shop, ref int nextSlot)
+        {
+            shop.item[nextSlot++].SetDefaults(ModContent.ItemType<EnchantedBait>());
+        }
+
+        public override void NPCLoot()
+        {
+            Item.NewItem(npc.getRect(), ModContent.ItemType<EnchantedBait>());
+        }
+
+        public override void TownNPCAttackStrength(ref int damage, ref float knockback)
+        {
+            damage = 30;
+            knockback = 4f;
+        }
+    }
+}
