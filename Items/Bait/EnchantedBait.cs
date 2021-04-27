2c98559b11581c50823888ae184325dd50ffd46d
Joel Kerfoot <joelkerfoot@gmail.com>
2021-04-26 22:48:02 -0700

    feat: add Enchanted Bait

---
+++ Items/Bait/EnchantedBait.cs
@@ -0,0 +1,29 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace BetterFishing.Items.Bait
+{
+    public class EnchantedBait : ModItem
+    {
+        
+        public override void SetDefaults()
+        {
+            item.maxStack = 999;
+            item.value = Item.sellPrice(0, 1, 0, 0);
+            item.rare = 4;
+            item.bait = 100;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.EnchantedNightcrawler, 4);
+            recipe.AddIngredient(ItemID.SoulofLight, 1);
+            recipe.AddTile(TileID.Campfire);
+
+            recipe.SetResult(this, 4);
+            recipe.AddRecipe();
+        }
+    }
+}
